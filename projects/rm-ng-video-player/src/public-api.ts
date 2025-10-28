2d7efb8c73bfab159943ad72b4b04f673b72b44d
Rajat <11173135+malikrajat@users.noreply.github.com>
2025-10-28 06:44:25 +0530

    Create public-api.ts

---
+++ projects/rm-ng-video-player/src/public-api.ts
@@ -0,0 +1,15 @@
+/*
+ * Public API Surface of rm-ng-video-player
+ */
+
+// Main component
+export * from './lib/rm-ng-video-player';
+
+// Interfaces
+export * from './lib/interfaces/video-player.interface';
+
+// Services
+export * from './lib/services/video-player.service';
+
+// Components
+export * from './lib/components/video-progress/video-progress.component';
